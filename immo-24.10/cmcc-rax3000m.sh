d1f4e6cfb699698034656420841365c541beecac
0x032c <0x032c@users.noreply.github.com>
2026-05-09 08:46:50 +0000

    style: keep default hostname 'ImmortalWrt' for all builds

--- immo-24.10/cmcc-rax3000m.sh
+++ immo-24.10/cmcc-rax3000m.sh
@@ -3,8 +3,6 @@
 ## 修改默认登录地址
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-## 修改默认主机名
-sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 
 rm -rf package/new
 mkdir -p package/new
